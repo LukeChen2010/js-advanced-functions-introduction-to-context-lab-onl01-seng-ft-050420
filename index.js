d4389d4531c10034ee410a685a23bf6d3cc8ad98
Luke Chen <luke.chen.2010@gmail.com>
2020-08-03 02:41:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,3 +58,15 @@ let wagesEarnedOnDate = function(employee, dateSought){
         * employee.payPerHour
     return parseFloat(rawWage.toString())
 }
+
+let allWagesFor = function(employee){
+    let eligibleDates = employee.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate(employee, d)
+    }, 0)
+
+    return payable
+}
